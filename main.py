81a2d031e3b7df10edd64273f3f0563300ae7581
xhmyjae <tomatis.mt@gmail.com>
2023-10-04 17:10:41 +0200

    feat(main): fixed main from merge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, render_template
-# from flask_mysqldb import MySQL
-import json
+from backend.api import DataBase
+
+
+db = DataBase('root', '', 'localhost', '3306', 'trashtalk')
+
+db.connect()
 
 app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
 
@@ -21,9 +25,10 @@ def get_data_by_id(product_id):
     return render_template('product_page.jinja', product=item)
 
 
+# cart page, needs all products in cart from the local storage
 @app.route('/cart', methods=['GET'])
 def get_cart():
-    return render_template('cart_page.html')
+    return render_template('cart_page.jinja')
 
 
 if __name__ == "__main__":
