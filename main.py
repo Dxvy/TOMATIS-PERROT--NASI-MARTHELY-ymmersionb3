44d97b3c2ac10d8592d547ddf1eb87bbbfcb996b
xhmyjae <tomatis.mt@gmail.com>
2023-10-04 14:36:29 +0200

    feat(database): api functions for product data

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 from flask import Flask, jsonify, render_template
-import sqlalchemy as db
-import json
+from backend.api import DataBase
+
+
+db = DataBase('root', '', 'localhost', '3306', 'trashtalk')
+
+db.connect()
 
 app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
 
-schema = 'trashcans'
-engine = db.create_engine(f'mysql://root:root@localhost:3306/{schema}')
-metadata = db.MetaData(schema=schema)
-
-
-# with open("backend/trashcans.json", "r") as read_file:
-#     data = json.load(read_file)
-
-
+# main route, needs all products
+# TODO: add recommendation system and search bar
 @app.route('/', methods=['GET'])
-def get_all_products_data():
-    # cursor = mysql.connection.cursor()
-    # cursor.execute("SELECT * FROM products")
-    # data = cursor.fetchall()
-    # cursor.close()
-    # return render_template('index.html', products=data)
-    return render_template('index.html', products=data)
+def display_all_products_data():
+    items = db.get_all_products()
+    return render_template('homepage.jinja', products=items)
 
 
-# @app.route('/<int:product_id>', methods=['GET'])
-# def get_data_by_id(product_id):
-#     item = next((item for item in data if item["product_id"] == product_id), None)
-#     if item is None:
-#         return render_template('error_page.html')
-#     return render_template('product_page.html', product=item)
+# product route, needs all product info
+@app.route('/<int:product_id>', methods=['GET'])
+def get_data_by_id(product_id):
+    item = db.get_product_by_id(product_id)
+    return render_template('product_page.jinja', product=item)
 
 
+# cart page, needs all products in cart from the local storage
 @app.route('/cart', methods=['GET'])
 def get_cart():
-    return render_template('cart_page.html')
+    return render_template('cart_page.jinja')
 
 
 if __name__ == "__main__":
