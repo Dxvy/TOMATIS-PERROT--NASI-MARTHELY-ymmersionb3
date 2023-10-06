ca4aa44b4dbac458b1208b436296dd2678741fbf
xhmyjae <tomatis.mt@gmail.com>
2023-10-06 13:56:53 +0200

    fix(*): *

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from werkzeug.security import generate_password_hash
 from backend.api import DataBase
 from backend.utils import *
